00dc66c0bbad4ce7e2cc1f503cc6ef6fb2091333
Dadaso <dada.z888@gmail.com>
2016-07-03 19:54:23 +0530

    added mysql config

--- plots/settings.py
+++ plots/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'plots.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'plots',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
